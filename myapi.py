bef8c6e71832f02c413c13653c0d1c703291ee81
ajrianop <ajrianop@unal.com>
2024-03-17 14:20:58 -0500

    creating informative commands

--- myapi.py
+++ myapi.py
@@ -1,16 +1,30 @@
 # Builnding our first API
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 # We create an instance of the API object
 app = FastAPI()
 
+students = {
+    1 : {
+        "name" : "karl",
+        "age" : "11",
+        "class" : "year 6"
+    },
+    2 : {
+        "name" : "karl",
+        "age" : "11",
+        "class" : "year 6"
+    }
+}
+
 # We create and endopoint (URL)
-
 @app.get("/")
-
 def index():
     '''
-
     The return is a normal JSON
     '''
     return {"name": "First Data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id : int = Path(None, description="The ID of the student you want to view" , gt=0 )):
+    return students[student_id]
